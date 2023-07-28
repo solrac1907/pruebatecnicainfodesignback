23e2260c2907bc32f58e42e1e4db72524e052a25
Carlos Taborda <carlos.taborda@clovit.com>
2023-07-28 14:35:15 -0500

    add cors and urlencoded

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const morgan = require('morgan');
-const { mysqlConn } = require('./database-MySQL');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 //Middleware
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
 //Routes
 app.use(require('./routes/routes'));
 
